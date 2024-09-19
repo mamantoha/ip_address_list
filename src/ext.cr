65d862d5d82aa9ce03a21afbfe671016c85d9eb8
Anton Maminov <anton.linux@gmail.com>
2024-09-19 05:49:16 -0700

    change windows implementation

--- src/ext.cr
+++ src/ext.cr
@@ -29,17 +29,12 @@ require "lib_c"
       adapter_name : Pointer(UInt8)
       first_unicast_address : Pointer(Void)
     end
-
-    struct SOCKADDR
-      sa_family : UInt16
-      sa_data : StaticArray(UInt8, 28)
-    end
-
+  
     struct IP_ADAPTER_UNICAST_ADDRESS
       length : UInt32
       flags : UInt32
       next : Pointer(IP_ADAPTER_UNICAST_ADDRESS)
-      address : Pointer(LibC::SOCKADDR)
+      address : Pointer(LibC::Sockaddr)
     end
 
     # https://learn.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getadaptersaddresses
@@ -103,32 +98,30 @@ class Socket
 
       while adapter
         unicast_address = adapter.value.first_unicast_address.as(Pointer(LibC::IP_ADAPTER_UNICAST_ADDRESS))
-
+  
         while unicast_address
-          sockaddr = unicast_address.value.address.as(Pointer(LibC::SOCKADDR))
-
-          if sockaddr.value.sa_family == 2 # AF_INET = 2 (IPv4)
-            ipv4_addr = sockaddr.value.sa_data.to_a[2, 4]
-            address = ipv4_addr.join('.')
+          sockaddr = unicast_address.value.address.as(Pointer(LibC::Sockaddr))
+  
+          if sockaddr.value.sa_family == 2  # AF_INET = 2 (IPv4)
+            sockaddr_in = sockaddr.as(Pointer(LibC::SockaddrIn))
+            address = "#{sockaddr_in.value.sin_addr.s_addr & 0xFF}.#{(sockaddr_in.value.sin_addr.s_addr >> 8) & 0xFF}.#{(sockaddr_in.value.sin_addr.s_addr >> 16) & 0xFF}.#{sockaddr_in.value.sin_addr.s_addr >> 24}"
             ip_address = Socket::IPAddress.new(address, 0)
-
+  
             list << ip_address
-          elsif sockaddr.value.sa_family == 23 # AF_INET6 = 23 (IPv6)
-            ipv6_addr = sockaddr.value.sa_data.to_a[6, 16]
-
+          elsif sockaddr.value.sa_family == 23  # AF_INET6 = 23 (IPv6)
+            sockaddr_in6 = sockaddr.as(Pointer(LibC::SockaddrIn6))
+            ipv6_addr = sockaddr_in6.value.sin6_addr.u.byte
             address = ipv6_addr.each_slice(2).map { |slice| "%02x%02x" % slice }.join(':')
-
             ip_address = Socket::IPAddress.new(address, 0)
-
+  
             list << ip_address
           end
-
+  
           unicast_address = unicast_address.value.next
         end
-
+  
         adapter = adapter.value.next
       end
-
       list
     end
   {% end %}
