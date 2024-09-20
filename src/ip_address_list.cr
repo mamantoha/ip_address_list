2c36b9536d45343659b151a67c87832d05861962
Anton Maminov <anton.linux@gmail.com>
2024-09-20 12:30:03 +0300

    remove unused code

--- src/ip_address_list.cr
+++ src/ip_address_list.cr
@@ -11,13 +11,3 @@ require "lib_c"
 
 require "./ext/socket"
 require "./ext/socket/address"
-
-module IpAddressList
-  VERSION = "0.1.0"
-
-  def self.local_ip_address : Socket::IPAddress?
-    Socket.ip_address_list.find do |ip_address|
-      ip_address.family == Socket::Family::INET && ip_address.private? && !ip_address.address.starts_with?("127")
-    end
-  end
-end
